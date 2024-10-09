573a8b68a97c43de829357730d48418b0c064da3
xylonity <loldanielespia@gmail.com>
2024-10-09 18:39:00 +0200

    :frog: Added froggy and custom path navigator

--- forge/src/main/java/dev/xylonity/companions/common/entity/FroggyEntity.java
+++ forge/src/main/java/dev/xylonity/companions/common/entity/FroggyEntity.java
@@ -1,13 +1,29 @@
 package dev.xylonity.companions.common.entity;
 
+import dev.xylonity.companions.common.ai.navigator.GroundNavigator;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.OwnerHurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.OwnerHurtTargetGoal;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.ForgeEventFactory;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
@@ -18,13 +34,120 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class FroggyEntity extends TamableAnimal implements GeoEntity {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
+    private static final EntityDataAccessor<Boolean> SITTING = SynchedEntityData.defineId(FroggyEntity.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> SIT_VARIATION = SynchedEntityData.defineId(FroggyEntity.class, EntityDataSerializers.INT);
+
     public FroggyEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
+    @Override
+    protected @NotNull PathNavigation createNavigation(@NotNull Level pLevel) {
+        return new GroundNavigator(this, pLevel);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
+        this.goalSelector.addGoal(4, new FollowOwnerGoal(this, 0.6D, 6.0F, 2.0F, false));
+        this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
+    }
+
+    @Override
+    public @NotNull InteractionResult mobInteract(Player player, @NotNull InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        Item itemForTaming = Items.APPLE;
+        Item item = itemstack.getItem();
+
+        if (item == itemForTaming && !isTame()) {
+            if (this.level().isClientSide) {
+                return InteractionResult.CONSUME;
+            } else {
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+                if (!ForgeEventFactory.onAnimalTame(this, player)) {
+                    if (!this.level().isClientSide) {
+                        super.tame(player);
+                        this.navigation.recomputePath();
+                        this.setTarget(null);
+                        this.level().broadcastEntityEvent(this, (byte)7);
+                        setSitting(true);
+                    }
+                }
+
+                setSitVariation(getRandom().nextInt(0, 3));
+
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        /*
+         * Handles the actions when the Samhain is tamed.
+         */
+
+        if (isTame() && !this.level().isClientSide && hand == InteractionHand.MAIN_HAND && getOwner() == player) {
+            if ((itemstack.getItem().equals(Items.APPLE) || itemstack.getItem().equals(Items.APPLE))
+                    && this.getHealth() < this.getMaxHealth()) {
+
+                if (itemstack.getItem().equals(Items.APPLE)) {
+                    this.heal(16.0F);
+                } else if (itemstack.getItem().equals(Items.APPLE)) {
+                    this.heal(4.0F);
+                }
+
+                if (!player.getAbilities().instabuild) {
+                    itemstack.shrink(1);
+                }
+
+            } else {
+                setSitting(!isSitting());
+                setSitVariation(getRandom().nextInt(0, 3));
+            }
+
+            return InteractionResult.SUCCESS;
+        }
+
+        if (itemstack.getItem() == itemForTaming) {
+            return InteractionResult.PASS;
+        }
+
+        return super.mobInteract(player, hand);
+    }
+
+    public void setSitting(boolean sitting) {
+        this.entityData.set(SITTING, sitting);
+        this.setOrderedToSit(sitting);
+    }
+
+    public boolean isSitting() {
+        return this.entityData.get(SITTING);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(SITTING, false);
+        this.entityData.define(SIT_VARIATION, 0);
+    }
+
+    private void setSitVariation(int sitVariation) {
+        this.entityData.set(SIT_VARIATION, sitVariation);
+    }
+
+    private int getSitVariation() {
+        return this.entityData.get(SIT_VARIATION);
+    }
+
     @Nullable
     @Override
-    public AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+    public AgeableMob getBreedOffspring(@NotNull ServerLevel serverLevel, @NotNull AgeableMob ageableMob) {
         return null;
     }
 
@@ -35,7 +158,10 @@ public class FroggyEntity extends TamableAnimal implements GeoEntity {
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> event) {
 
-        if (event.isMoving()) {
+        if (this.isSitting()) {
+            String sitVariation = getSitVariation() == 0 ? "sit1" : getSitVariation() == 1 ? "sit3" : "sit2";
+            event.getController().setAnimation(RawAnimation.begin().then(sitVariation, Animation.LoopType.LOOP));
+        } else if (event.isMoving()) {
             event.getController().setAnimation(RawAnimation.begin().then("walk", Animation.LoopType.LOOP));
         } else {
             event.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
@@ -46,10 +172,10 @@ public class FroggyEntity extends TamableAnimal implements GeoEntity {
 
     public static AttributeSupplier setAttributes() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 1.0D)
+                .add(Attributes.MAX_HEALTH, 10.0D)
                 .add(Attributes.ATTACK_DAMAGE, 5f)
                 .add(Attributes.ATTACK_SPEED, 1.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.63f)
+                .add(Attributes.MOVEMENT_SPEED, 0.55f)
                 .add(Attributes.FOLLOW_RANGE, 35.0).build();
     }
 
