5c15e7ffc0c9f5661dcc55b459ae2e17580048ba
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-04-15 04:06:04 +0200

    💴 Basic impl of the voodoo status effect

--- common/src/main/java/dev/xylonity/companions/registry/CompanionsEffects.java
+++ common/src/main/java/dev/xylonity/companions/registry/CompanionsEffects.java
@@ -3,6 +3,7 @@ package dev.xylonity.companions.registry;
 import dev.xylonity.companions.CompanionsCommon;
 import dev.xylonity.companions.common.effect.ElectroshockEffect;
 import dev.xylonity.companions.common.effect.FireMarkEffect;
+import dev.xylonity.companions.common.effect.VoodooEffect;
 import net.minecraft.world.effect.MobEffect;
 
 import java.util.function.Supplier;
@@ -13,6 +14,7 @@ public class CompanionsEffects {
 
     public static final Supplier<MobEffect> FIRE_MARK = registerEffect("fire_mark", FireMarkEffect::new);
     public static final Supplier<MobEffect> ELECTROSHOCK = registerEffect("electroshock", ElectroshockEffect::new);
+    public static final Supplier<MobEffect> VOODOO = registerEffect("voodoo", VoodooEffect::new);
 
     private static <T extends MobEffect> Supplier<T> registerEffect(String id, Supplier<T> effect) {
         return CompanionsCommon.COMMON_PLATFORM.registerEffect(id, effect);
