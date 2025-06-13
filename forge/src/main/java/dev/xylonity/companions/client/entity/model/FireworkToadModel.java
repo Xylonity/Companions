d02876d0905c02e9355de9ed091840577739f7d1
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-06-13 15:31:14 +0200

    🍐 Updated firework toad model

--- forge/src/main/java/dev/xylonity/companions/client/entity/model/FireworkToadModel.java
+++ forge/src/main/java/dev/xylonity/companions/client/entity/model/FireworkToadModel.java
@@ -1,7 +1,6 @@
 package dev.xylonity.companions.client.entity.model;
 
 import dev.xylonity.companions.CompanionsCommon;
-import dev.xylonity.companions.common.entity.custom.AntlionEntity;
 import dev.xylonity.companions.common.entity.summon.FireworkToadEntity;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
@@ -10,17 +9,17 @@ public class FireworkToadModel extends GeoModel<FireworkToadEntity> {
 
     @Override
     public ResourceLocation getModelResource(FireworkToadEntity animatable) {
-        return new ResourceLocation(CompanionsCommon.MOD_ID, "geo/froggy.geo.json");
+        return new ResourceLocation(CompanionsCommon.MOD_ID, "geo/firework_toad.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(FireworkToadEntity animatable) {
-        return new ResourceLocation(CompanionsCommon.MOD_ID, "textures/entity/froggy.png");
+        return new ResourceLocation(CompanionsCommon.MOD_ID, "textures/entity/firework_toad.png");
     }
 
     @Override
     public ResourceLocation getAnimationResource(FireworkToadEntity animatable) {
-        return new ResourceLocation(CompanionsCommon.MOD_ID, "animations/generic.animation.json");
+        return new ResourceLocation(CompanionsCommon.MOD_ID, "animations/firework_toad.animation.json");
     }
 
 }
