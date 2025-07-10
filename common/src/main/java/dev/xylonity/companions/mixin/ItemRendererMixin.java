6330ebc2830b2e78809ed958c54d88d20b9b7a7b
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-07-10 15:21:46 +0200

    🐛 Fixed 3d in hand weapon item rendering

---
+++ common/src/main/java/dev/xylonity/companions/mixin/ItemRendererMixin.java
@@ -0,0 +1,76 @@
+package dev.xylonity.companions.mixin;
+
+import dev.xylonity.companions.CompanionsCommon;
+import dev.xylonity.companions.registry.CompanionsItems;
+import net.minecraft.client.renderer.ItemModelShaper;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.Map;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+
+    @Shadow
+    @Final
+    private ItemModelShaper itemModelShaper;
+
+
+    @Unique
+    private static final Map<Item, ModelResourceLocation> companions$MODELS_3D;
+
+    @Unique
+    private static final Map<Item, ModelResourceLocation> companions$MODELS_2D;
+
+    @ModifyVariable(method = "getModel", at = @At(value = "STORE"), ordinal = 1)
+    public BakedModel getModel(BakedModel bakedModel, ItemStack pStack) {
+        ModelResourceLocation loc = companions$MODELS_3D.get(pStack.getItem());
+        if (loc != null) {
+            return this.itemModelShaper.getModelManager().getModel(loc);
+        }
+
+        return bakedModel;
+    }
+
+    @ModifyVariable(method = "render", at = @At("HEAD"), argsOnly = true)
+    private BakedModel replaceBaked(BakedModel bakedModel, ItemStack pStack, ItemDisplayContext pDisplayContext) {
+        boolean ctx = switch (pDisplayContext) {
+            case GUI, GROUND, FIXED -> true;
+            default -> false;
+        };
+
+        if (ctx) {
+            ModelResourceLocation loc = companions$MODELS_2D.get(pStack.getItem());
+            if (loc != null) {
+                return this.itemModelShaper.getModelManager().getModel(loc);
+            }
+        }
+
+        return bakedModel;
+    }
+
+    static {
+        companions$MODELS_3D = Map.of(
+                CompanionsItems.CRYSTALLIZED_BLOOD_AXE.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_axe_3d",   "inventory"),
+                CompanionsItems.CRYSTALLIZED_BLOOD_SCYTHE.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_scythe_3d","inventory"),
+                CompanionsItems.CRYSTALLIZED_BLOOD_SWORD.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_sword_3d", "inventory")
+        );
+        companions$MODELS_2D = Map.of(
+                CompanionsItems.CRYSTALLIZED_BLOOD_AXE.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_axe",   "inventory"),
+                CompanionsItems.CRYSTALLIZED_BLOOD_SCYTHE.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_scythe","inventory"),
+                CompanionsItems.CRYSTALLIZED_BLOOD_SWORD.get(), new ModelResourceLocation(CompanionsCommon.MOD_ID, "crystallized_blood_sword", "inventory")
+        );
+
+    }
+
+}
