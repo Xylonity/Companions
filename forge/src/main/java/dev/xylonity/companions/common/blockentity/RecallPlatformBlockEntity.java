bf7c2c861b93ca0b50845d351d1a530c8e3568f1
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-07-26 01:57:11 +0200

    🎻 Now recall platform sounds

--- forge/src/main/java/dev/xylonity/companions/common/blockentity/RecallPlatformBlockEntity.java
+++ forge/src/main/java/dev/xylonity/companions/common/blockentity/RecallPlatformBlockEntity.java
@@ -1,19 +1,21 @@
 package dev.xylonity.companions.common.blockentity;
 
 import dev.xylonity.companions.common.tesla.TeslaConnectionManager;
-import dev.xylonity.companions.common.tesla.behaviour.lamp.LampPulseBehaviour;
 import dev.xylonity.companions.common.tesla.behaviour.platform.RecallPlatformPulseBehaviour;
 import dev.xylonity.companions.common.util.interfaces.ITeslaNodeBehaviour;
 import dev.xylonity.companions.registry.CompanionsBlockEntities;
+import dev.xylonity.companions.registry.CompanionsSounds;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,9 +37,14 @@ public class RecallPlatformBlockEntity extends AbstractTeslaBlockEntity {
     public static <T extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState blockState, T t) {
         if (!(t instanceof RecallPlatformBlockEntity platform)) return;
 
+        if (level.getEntitiesOfClass(Player.class, new AABB(platform.getBlockPos()).move(0, 1, 0).inflate(0.5)).isEmpty()) {
+            platform.cooldown = COOLDOWN_TICKS;
+        } else {
+            platform.cooldown--;
+        }
+
         platform.pulseBehaviour.process(platform, level, blockPos, blockState);
 
-        if (platform.cooldown > 0) platform.cooldown--;
         platform.tickCount++;
 
         platform.sync();
@@ -65,7 +72,11 @@ public class RecallPlatformBlockEntity extends AbstractTeslaBlockEntity {
                 continue;
             }
 
+            if (level != null) level.playSound(null, getBlockPos(), CompanionsSounds.TEDDY_TRANSFORMATION.get(), SoundSource.BLOCKS, 1, 1);
+
             player.teleportTo(target.getX() + .5, target.getY() + 1, target.getZ() + .5);
+
+            if (level != null) level.playSound(null, target, CompanionsSounds.TEDDY_TRANSFORMATION.get(), SoundSource.BLOCKS, 1, 1);
 
             this.cooldown = COOLDOWN_TICKS;
             otherPlatform.cooldown = COOLDOWN_TICKS;
