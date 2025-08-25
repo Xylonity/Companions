e555cc881c632730f629345538fc9314c7c9a0a6
xylonity <42015327+Xylonity@users.noreply.github.com>
2025-08-25 19:47:59 +0200

    🎁 Now the netherite chains can be visually appreciated when applied to the minion

--- forge/src/main/java/dev/xylonity/companions/client/entity/renderer/MinionRenderer.java
+++ forge/src/main/java/dev/xylonity/companions/client/entity/renderer/MinionRenderer.java
@@ -1,11 +1,13 @@
 package dev.xylonity.companions.client.entity.renderer;
 
-import dev.xylonity.companions.CompanionsCommon;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.xylonity.companions.client.entity.model.MinionModel;
 import dev.xylonity.companions.common.entity.companion.MinionEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
@@ -15,6 +17,15 @@ public class MinionRenderer extends GeoEntityRenderer<MinionEntity> {
         super(renderManager, new MinionModel());
         addRenderLayer(new AutoGlowingGeoLayer<>(this));
         this.shadowRadius = 0.7f;
+    }
+
+    @Override
+    public void renderRecursively(PoseStack poseStack, MinionEntity animatable, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        if (bone.getName().contains("chain") && !animatable.isPhaseLocked()) {
+            return;
+        }
+
+        super.renderRecursively(poseStack, animatable, bone, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 
 }
