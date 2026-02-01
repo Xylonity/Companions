6641e87f1e31664403421c98891ac0020669fc86
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-02-01 04:00:46 +0100

    🐛 Attempt to fix #51

--- forge/src/main/java/dev/xylonity/companions/common/blockentity/RecallPlatformBlockEntity.java
+++ forge/src/main/java/dev/xylonity/companions/common/blockentity/RecallPlatformBlockEntity.java
@@ -85,9 +85,12 @@ public class RecallPlatformBlockEntity extends AbstractTeslaBlockEntity {
     }
 
     public void updatePartners(Set<BlockPos> newPartners) {
+        if (partnerPositions.equals(newPartners)) {
+            return;
+        }
+
         partnerPositions.clear();
         partnerPositions.addAll(newPartners);
-        setChanged();
     }
 
     @Override
